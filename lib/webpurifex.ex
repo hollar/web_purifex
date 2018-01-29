71412a3793b8bc3e2d3e123ca83cdc05ee44a97d
Nicolas Charlery <nicolas@bokay.io>
2018-01-29 11:19:25 -0500

    Install base Elixir project

---
+++ lib/webpurifex.ex
@@ -0,0 +1,18 @@
+defmodule Webpurifex do
+  @moduledoc """
+  Documentation for Webpurifex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Webpurifex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/webpurifex_test.exs
@@ -0,0 +1,8 @@
+defmodule WebpurifexTest do
+  use ExUnit.Case
+  doctest Webpurifex
+
+  test "greets the world" do
+    assert Webpurifex.hello() == :world
+  end
+end
